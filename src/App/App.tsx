9c99b9efa66a7f6124db58e85c7b1cef6a6db84c
Oksana Aleksandrova <102592873+oks767@users.noreply.github.com>
2024-09-21 08:19:21 +0300

    Созданы компоненты, отрисована главная страница

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "../Home/Home";
+
+const App = () => {
+    return(
+        <>
+            <div className="page page--gray page--main">
+                <Home cardItems={312}/>
+            </div>
+        </>
+    )
+}
+
+export default App
