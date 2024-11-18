1d3499d78a8013870b249888a4a5a1a9c3cdf07a
Oksana Aleksandrova <102592873+oks767@users.noreply.github.com>
2024-11-18 10:19:47 +0300

    пока не работает, нужно сделать авторизацию

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,24 +1,56 @@
-import { FC } from 'react';
+
 import Card from '../card/card.tsx';
 import Header from '../header/header.tsx';
 import CityList from '../сity-list/city-list.tsx';
 import { offersListData } from '../mocks/offers-list.ts';
 import Map from '../map/map.tsx';
+import { CityType, OffersType } from '../types/offers.ts';
+import {  useState } from 'react';
+import { offers } from '../mocks/offers.ts';
+import cn from 'classnames'
 
-type CardProps = {
-    cardItems: React.ReactNode;
+// type CardProps = {
+//     cardItems: React.ReactNode;
+// }
+type MainScreenProps = {
+  offers: OffersType;
 }
 
-const Home: FC<CardProps> = ({cardItems}): JSX.Element => (
+const filterOffersByCity = (offers: OffersType, city: string): OffersType => (
+  offers.filter((offer) => offer.city.name === city)
+);
+
+// const findCityInfo = (offers: OffersType, activeTab: string): CityType => (
+//   offers.find((offer) => offer.city.name === activeTab).city
+// );
+// компилятор TS не устраивала ф-ция find, пришлось писать так
+const findCityInfo = (offers: OffersType, activeTab: string): CityType => (
+  filterOffersByCity(offers, activeTab)[0].city
+);
+
+const Home = ({offers}: MainScreenProps): JSX.Element => (
+  const [activeTab, setActiveTab] = useState('');
+  const [filteredOffers, setFilteredOffers] = useState<OffersType>([]);
+  const [activeOfferID, setActiveOfferID] = useState<number | null>(null);
+
+  const getTitle = (numberOfOffers: number) => (
+    `${numberOfOffers} ${numberOfOffers === 1 ? 'place' : 'places'} to stay in ${activeTab}`
+  );
+
+  const mainClassName = cn('page__main page__main--index', {
+    'page__main--index-empty': Boolean(filteredOffers.length),
+  });
+
+
   <>
     <Header/>
-    <main className="page__main page__main--index">
-      <CityList />
+    <main className={mainClassName}>
+      <CityList offers={offers}/>
       <div className="cities">
         <div className="cities__places-container container">
           <section className="cities__places places">
             <h2 className="visually-hidden">Places</h2>
-            <b className="places__found">{cardItems} places to stay in Amsterdam</b>
+            <b className="places__found"> places to stay in Amsterdam</b>
             <form className="places__sorting" action="#" method="get">
               <span className="places__sorting-caption">Sort by</span>
               <span className="places__sorting-type">
@@ -38,7 +70,12 @@ const Home: FC<CardProps> = ({cardItems}): JSX.Element => (
               <Card offersData={offersListData}/>
             </div>
           </section>
-          <Map city={offersListData}/>
+          <Map
+            cityInfo={findCityInfo(offers, activeTab)}
+            points={filteredOffers}
+            activeOfferID={activeOfferID}
+            screenClass={Screen.main}
+          />
         </div>
       </div>
     </main>
--- src/сity-list/city-list.tsx
+++ src/сity-list/city-list.tsx
@@ -1,42 +1,36 @@
+import { useLayoutEffect, useState } from "react";
+import { CITIES } from "../constans";
+import { OffersType } from "../types/offers";
+import { offers } from "../mocks/offers";
+import TabItem from "./tab-item";
+type MainScreenProps = {
+  offers: OffersType,
+}
 
-
-const CityList = (): JSX.Element => (
+const CityList = ({offers}: MainScreenProps): JSX.Element => (
+  const [activeTab, setActiveTab] = useState('');
+  const [filteredOffers, setFilteredOffers] = useState<OffersType>([]);
+  useLayoutEffect(() => {
+    setActiveTab('Amsterdam');
+    setFilteredOffers(filterOffersByCity(offers, 'Amsterdam'));
+  }, [offers]);
+  const handleTabClick = (city: string) => {
+    setActiveTab(city);
+    setFilteredOffers(filterOffersByCity(offers, city));
+  };
   <>
     <h1 className="visually-hidden">Cities</h1>
     <div className="tabs">
       <section className="locations container">
-        <ul className="locations__list tabs__list">
-          <li className="locations__item">
-            <a className="locations__item-link tabs__item" href="#">
-              <span>Paris</span>
-            </a>
-          </li>
-          <li className="locations__item">
-            <a className="locations__item-link tabs__item" href="#">
-              <span>Cologne</span>
-            </a>
-          </li>
-          <li className="locations__item">
-            <a className="locations__item-link tabs__item" href="#">
-              <span>Brussels</span>
-            </a>
-          </li>
-          <li className="locations__item">
-            <a className="locations__item-link tabs__item tabs__item--active">
-              <span>Amsterdam</span>
-            </a>
-          </li>
-          <li className="locations__item">
-            <a className="locations__item-link tabs__item" href="#">
-              <span>Hamburg</span>
-            </a>
-          </li>
-          <li className="locations__item">
-            <a className="locations__item-link tabs__item" href="#">
-              <span>Dusseldorf</span>
-            </a>
-          </li>
-        </ul>
+      <ul className="locations__list tabs__list">
+              {CITIES.map((city) => (
+                <TabItem
+                  city={city}
+                  key={city}
+                  onTabClick={handleTabClick}
+                  activeTab={activeTab}
+                />))}
+            </ul>
       </section>
     </div>
   </>
