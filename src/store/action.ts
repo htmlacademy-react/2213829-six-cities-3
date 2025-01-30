301dfaf562806c60b7bfc620e242ef37f0a7057d
Oksana Aleksandrova <102592873+oks767@users.noreply.github.com>
2025-01-30 16:15:54 +0300

    add function

--- src/store/action.ts
+++ src/store/action.ts
@@ -5,3 +5,7 @@ import {Cities} from '../const.ts';
 export const changeCity = createAction<Cities>('city/change');
 
 export const fillOffers = createAction<Offer[]>('offers/fill');
+export const setError = (message: string) => ({
+    type: 'SET_ERROR',
+    payload: message,
+  });
